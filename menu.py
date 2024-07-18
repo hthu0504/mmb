fa6ca12fa22a39cb032e6998e9c13c2e66246ce0
hthu0504 <nguyenhoanganhthu18101984@gmail.com>
2024-07-18 18:02:37 +0700

    alter

--- menu.py
+++ menu.py
@@ -17,28 +17,22 @@ def main():
         
         choice = input("Chọn tùy chọn: ")
         
-        hi = manage()
         if choice == '1':
             name = input("Nhập tên học sinh: ")
             lop = input("Nhập lớp: ")
             birth = input("Nhập năm sinh: ")
-            hi.AddStudent(name, lop, birth)
         
         elif choice == '2':
             name = input('Nhập tên học sinh: ')
             birth = input('Nhập năm sinh: ')
-            index = hi.Check(hi.student, name, birth)
             if index == -1:
                 print('Không có học sinh này trong lớp')
             else:
-                while True:
-                    hi.RemoveStudent(name, birth, lop)
 
         elif choice == '3':
             name = input("Nhập tên học sinh: ")
             lop = input("Nhập lớp: ")
             birth = input('Năm sinh: ')
-            index = hi.Check(hi.student, birth, lop)
             if index == -1:
                 print('Không có học sinh này trong lớp')
             else:
@@ -48,13 +42,10 @@ def main():
                         break
                     elif key == 'lớp':
                         new_value = input('Nhập lớp cần cập nhật: ')
-                        hi.student[index].lop = new_value
                     elif key == 'năm sinh':
                         new_value = input('Nhập năm sinh cần cập nhật: ')
-                        hi.student[index].birth = new_value
                     else:
                         new_value = input(f'Nhập giá trị mới cho {key}: ')
-                        hi.student[index].details[key] = new_value
                     print('Thông tin học sinh đã được cập nhật')
         
         elif choice == '4':
@@ -62,22 +53,17 @@ def main():
             subj = input("Nhập môn học: ")
             lop = input("Nhập lớp: ")
             birth = input("Nhập năm sinh: ")
-            hi.AddTeacher(name, subj, lop, birth)
 
         elif choice == '5':
             name = input('Nhập tên giáo viên: ')
             birth = input('Nhập năm sinh: ')
-            index = hi.Check(hi.teacher, name, birth)
             if index == -1:
                 print('Không có giáo viên này trong lớp')
             else:
-                while True:
-                    hi.RemoveTeacher(name, birth, lop)
 
         elif choice == '6':
             name = input('Nhập tên giáo viên: ')
             birth = input('Nhập năm sinh: ')
-            index = hi.Check(hi.teacher, name, birth)
             if index == -1:
                 print('Không có giáo viên này trong lớp')
             else:
@@ -87,24 +73,19 @@ def main():
                         break
                     elif key == 'lớp':
                         new_value = input('Nhập lớp cần cập nhật: ')
-                        hi.teacher[index].lop = new_value
                     elif key == 'năm sinh':
                         new_value = input('Nhập năm sinh cần cập nhật: ')
-                        hi.teacher[index].birth = new_value
                     else:
                         new_value = input(f'Nhập giá trị mới cho {key}: ')
-                        hi.teacher[index].details[key] = new_value
                     print('Thông tin giáo viên đã được cập nhật')
 
         elif choice == '7':
             name = input('Nhập tên lớp: ')
             khoa = input('Nhập khóa: ')
-            hi.AddClass(name, khoa)
 
         elif choice == '8':
             name = input('Xóa lớp: ')
             khoa = input('Nhập khóa: ')
-            hi.RemoveClass(name, khoa)
         
         elif choice == '9':
             print("Thoát chương trình")
